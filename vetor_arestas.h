83ae813c9fd3238b66580160c6ebb2a3dbe178e3
majurigotti <mariajuliarigotti05@gmail.com>
2026-05-05 14:46:36 -0300

    Tad vetor arestas comentado

--- vetor_arestas.h
+++ vetor_arestas.h
@@ -5,10 +5,26 @@
 
 typedef struct VetorArestas tVetorArestas;
 
+/*@param:
+    int numPontos: inteiro contendo a quantidade de pontos;
+@return: cria um vetor de arestas*/
 tVetorArestas* criaVetorArestas(int numPontos);
+
+/*@param: 
+    tVetorArestas *vetor: ponteiro pro vetor de arestas;
+    taresta *aresta: ponteiro pra aresta que será adicionada ao vetor;*/
 void adicionaAresta(tVetorArestas *vetor, tAresta *aresta);
+
+/*@param:
+    tVetorArestas *vetor: ponteiro pro vetor de arestas;*/
 void ordenaArestas(tVetorArestas *vetor);
+
+/*@param:
+    tVetorArestas *vetor: ponteiro pro vetor de arestas;*/
 void imprimeArestas(tVetorArestas *vetor);
+
+/*@param:
+    tVetorArestas *vetor: ponteiro pro vetor de arestas que será desalocado;*/
 void desalocaVetorArestas(tVetorArestas *vetor);
 
 #endif
