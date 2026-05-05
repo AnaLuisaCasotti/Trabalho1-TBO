a1eb9e0bff3d0afb7508c66d8cdde7100fda774e
majurigotti <mariajuliarigotti05@gmail.com>
2026-05-05 14:41:19 -0300

    Tad Aresta comentado
    Added function prototypes for initializing and managing edges.

--- aresta.h
+++ aresta.h
@@ -5,7 +5,14 @@
 
 typedef struct Aresta tAresta;
 
+/*@param: 
+    double dist: distância entre dois pontos;
+    tPonto *v1, *v2: o ponteiro para os dois pontos;
+@return: uma estrutura tAresta alocada na memória*/
 tAresta* inicializaAresta(double dist, tPonto *v1, tPonto *v2);
+
+/*@param: tAresta *aresta: o ponteiro para a aresta
+@return: um número double que representa a distância correspondente à aresta*/
 double retornaDistancia(tAresta *aresta);
 
 /*@param: tPonto *p: o ponteiro para o ponto p que será desalocado;*/
