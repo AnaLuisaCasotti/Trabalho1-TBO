1f296df46f88e3f4e3dba9c3adbe4e2fb846156d
majurigotti <mariajuliarigotti05@gmail.com>
2026-05-05 14:37:02 -0300

    Comentando o código
    Added parameter documentation for desalocaAresta function.

--- aresta.h
+++ aresta.h
@@ -7,6 +7,9 @@ typedef struct Aresta tAresta;
 
 tAresta* inicializaAresta(double dist, tPonto *v1, tPonto *v2);
 double retornaDistancia(tAresta *aresta);
+
+/*@param: tPonto *p: o ponteiro para o ponto p que será desalocado;*/
 void desalocaAresta(tAresta *aresta);
 
 #endif
+
